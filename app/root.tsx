56ab5abbd72a0599c0f2f39e7ac9dea3d3fdd3b4
Amila Jayakody <AmilaDarshanaJ@users.noreply.github.com>
2023-11-24 16:35:01 +0530

    Used useNavigation - Added global pending UI

--- app/root.tsx
+++ app/root.tsx
@@ -9,6 +9,7 @@ import {
   Scripts,
   ScrollRestoration,
   useLoaderData,
+  useNavigation,
 } from "@remix-run/react";
 
 import type { LinksFunction } from "@remix-run/node";
@@ -33,6 +34,7 @@ export const loader = async () => {
 export default function App() {
 
   const { contacts } = useLoaderData<typeof loader>();
+  const navigation = useNavigation();
 
   return (
     <html lang="en">
@@ -94,7 +96,10 @@ export default function App() {
             )}
           </nav>
         </div>
-        <div id="detail">
+        <div  className={
+            navigation.state === "loading" ? "loading" : ""
+          }
+           id="detail">
           <Outlet />
         </div>
 
